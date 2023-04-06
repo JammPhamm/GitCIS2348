606fc96910512c3219c6f368850560826f4b0909
jampham <62073330+jampham@users.noreply.github.com>
2023-04-05 20:13:45 -0500

    A

---
+++ Homework 3/10.17_zyLab.py
@@ -0,0 +1,7 @@
+#ItemToPurchase class
+class ItemToPurchase:
+    #constructor + 3 attributes
+    def __init__(self):
+        self.item_name="none"
+        self.item_price=0
+        self.item_quantity=0

5388dc3a1db36dd0b1bb9c76917df8dd63b0a141
jampham <62073330+jampham@users.noreply.github.com>
2023-04-05 19:52:40 -0500

    B

--- Homework 3/10.15_zyLab.py
+++ Homework 3/10.15_zyLab.py
@@ -1,6 +1,11 @@
+# Pham, James 10.15 zyLab
 class Team:
-    def init(self):
-        self.teamname = 'none'
-        self.teamwins = 0
+    def __init__(self):
+        self.team_name = 'none'
+        self.team_wins = 0
         self.team_losses = 0
+
+    #get win percentage
+    def get_win_percentage(self):
+        return self.team_wins / (self.team_wins + self.team_losses)
 

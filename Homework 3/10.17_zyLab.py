e3dccc84281338924dcb4ceaae415c7a8e98a1bc
jampham <62073330+jampham@users.noreply.github.com>
2023-04-05 20:20:35 -0500

    C

--- Homework 3/10.17_zyLab.py
+++ Homework 3/10.17_zyLab.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     item1.item_name = input('Enter the item name:\n')
     item1.item_price = int(input('Enter the item price:\n'))
     item1.item_quantity = int(input('Enter the item quantity:\n'))
-
+    print()
     # for item 2
     print('\nItem 2')
     item2.item_name = input('Enter the item name:\n')

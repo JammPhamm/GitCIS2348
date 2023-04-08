284784544fd4b6916c13156b1f3fe76ccb795928
jampham <62073330+jampham@users.noreply.github.com>
2023-04-08 17:32:01 -0500

    write test

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -4,17 +4,8 @@ import csv
 with open('ManufacturerList.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
-    for line in csv_reader:
-        print(line[1])
-print()
-with open('PriceList.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+    with open('new_ManufacturerList.csv', 'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter='-')
 
-    for line in csv_reader:
-        print(line[1])
-print()
-with open('ServiceDatesList.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    for line in csv_reader:
-        print(line[1])
+        for line in csv_reader:
+            csv_writer.writerow(line)

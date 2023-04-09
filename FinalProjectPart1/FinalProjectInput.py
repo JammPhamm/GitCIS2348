480c98eed688780e8519f140b22410aba9022eb0
jampham <62073330+jampham@users.noreply.github.com>
2023-04-08 21:19:57 -0500

    write test

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -2,10 +2,7 @@
 import csv
 
 with open('ManufacturerList.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+    csv_reader = csv.DictReader(csv_file)
 
-    with open('new_ManufacturerList.csv', 'w') as new_file:
-        csv_writer = csv.writer(new_file, delimiter='-')
-
-        for line in csv_reader:
-            csv_writer.writerow(line)
+    for line in csv_reader:
+        print(line['phone'])

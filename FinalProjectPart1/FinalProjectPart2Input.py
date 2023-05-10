cc767cce57246505bb43991eac0b19d7e8891db9
jampham <62073330+jampham@users.noreply.github.com>
2023-05-09 20:15:14 -0500

    2

--- FinalProjectPart1/FinalProjectPart2Input.py
+++ FinalProjectPart1/FinalProjectPart2Input.py
@@ -1,4 +1,3 @@
-# Pham, James 1823491 Final Project Part 2
 import csv
 
 # Read FullInventory.csv
@@ -16,16 +15,22 @@ item_type = input("Enter the item type: ").strip().lower()
 # Search for item in inventory
 found_items = []
 for item in inventory:
+    item_id = item[0]
     item_manufacturer = item[1].lower()
     item_item_type = item[2].lower()
-    if manufacturer in item_manufacturer and item_type in item_item_type:
-        found_items.append(item)
+    item_price = float(item[3])
+    item_service_date = item[4]
+    item_damaged = item[5].lower()
+    if manufacturer in item_manufacturer and item_type in item_item_type and item_service_date == "" and item_damaged == "no":
+        found_items.append([item_id, item_manufacturer, item_item_type, item_price])
 
 # Print search results
 if len(found_items) == 0:
     print("No such item in inventory")
-elif len(found_items) == 1:
-    print("Your item is: ")
-    print(found_items[0])
 else:
-    print("Multiple items found. Please enter a more specific query.")
+    most_expensive_item = found_items[0]
+    for item in found_items:
+        if item[3] > most_expensive_item[3]:
+            most_expensive_item = item
+    print("Your item is: ", most_expensive_item[0], most_expensive_item[1], most_expensive_item[2], most_expensive_item[3])
+
